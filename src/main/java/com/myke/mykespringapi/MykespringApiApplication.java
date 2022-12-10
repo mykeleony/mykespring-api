560ac69995d677599f4f330c907c3ee152219596
Myke Leony dos Santos Amorim <myke.amorim@ibm.com>
2022-12-09 22:22:26 -0300

    Initial commit

---
+++ src/main/java/com/myke/mykespringapi/MykespringApiApplication.java
@@ -0,0 +1,13 @@
+package com.myke.mykespringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MykespringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MykespringApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myke/mykespringapi/MykespringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myke.mykespringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MykespringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
